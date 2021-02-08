60a79c40fe0ee9fae7d3f5d613f77c65fe1f2417
Katy Day <day.katy@gmail.com>
2021-02-08 16:13:24 +0000

    creating javabuzz functionality and test

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -2,6 +2,39 @@ describe('', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+  
+  describe('when playing, says', function() {
+
+    it(' "Java" when a number is divisible by 3', function() {
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
+
+    it(' "Buzz" when a number is divisible by 5', function() {
+      expect(javabuzz.says(5)).toEqual("Buzz")
+    });
+
+    it(' "Javabuzz" when a number is divisible by 15', function() {
+      expect(javabuzz.says(15)).toEqual("Javabuzz")
+    });
+
+  });
+
+  describe('all other numbers', function() {
+
+    it('1 for 1', function() {
+      expect(javabuzz.says(1)).toEqual(1);
+    });
+
+      it('11 for 11', function() {
+        expect(javabuzz.says(11)).toEqual(11);
+      
+    });
+
+  });
+
 }); 
 
   
